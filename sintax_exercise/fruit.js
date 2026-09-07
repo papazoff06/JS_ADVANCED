51e710e07439a11f7c5bb6fbdfbf432ae6b09cc1
Anton Papazov <134053962+papazoff06@users.noreply.github.com>
2026-09-07 16:09:59 +0300

    Delete sintax_exercise directory

--- sintax_exercise/fruit.js
+++
@@ -1,9 +0,0 @@
-function calcFruit(typeFruit, weightFruit, priceFruit) {
-    let totalPrice = (weightFruit / 1000) * priceFruit;
-    console.log(`I need $${totalPrice.toFixed(2)} to buy ${ (weightFruit / 1000).toFixed(2) } kilograms ${typeFruit}.`);
-}
-
-
-
-calcFruit('orange', 2500, 1.80);
-calcFruit('apple', 1563, 2.350);
